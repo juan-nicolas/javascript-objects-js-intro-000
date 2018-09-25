cca6d4a876c749abd8e659651b3440a44d006b0d
Juan Nicolas <jnma91@gmail.com>
2018-09-25 23:58:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'Pink Floyd' : 'Time'}
 
-function updatePlay;ist(playlist, artistName, songTitle){
+function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = songTitle;
 }
