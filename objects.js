28ada7acda6bb3b2e8f7eee37c85b8f2ce5fc41f
Juan Nicolas <jnma91@gmail.com>
2018-09-26 00:00:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {'Pink Floyd' : 'Time'}
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+}
